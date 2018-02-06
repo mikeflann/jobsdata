c7f431abeb23171d1deed7372572e379f8adab20
Michael Flannelly <michael.flannelly2@gmail.com>
2018-02-06 15:12:59 -0500

    Create blscodes.py
    This is the start of dictionaries of various BLS codes for the CES, LAUS, and QCEW series

---
+++ blscodes.py
@@ -0,0 +1,31 @@
+# BLS Codes in Dictionaries
+
+series_prefix = {
+'CES - State and Metro': 'SM',
+'CES - National': 'CE',
+'QCEW': 'EN',
+'LAUS': 'LA',
+
+}
+
+state_code = {
+'Alabama': '01',
+'Alaska': '02',
+'Arizona': '04',
+}
+
+area_code = {
+'Statewide' = '00000',
+'Abilene, TX' = '10180',
+}
+
+supersector_code = {
+'Total Nonfarm': '00',
+'Total Private': '05', 
+
+}
+
+industry_code = {
+'Total Nonfarm' = '00000000',
+
+}
